b9564d5289aee98ad1056a9b26433722e032c9eb
M-Abdeltawab <131000624+M-Abdeltawab@users.noreply.github.com>
2023-04-22 00:32:01 +0200

    Update word_count.sh

--- word_count.sh
+++ word_count.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # using wc command to count number of words
-number_of_words=`wc --word < $file_path`
+number_of_words=`wc --word 
 
 # Displaying number of words
 echo "Number of words: $number_of_words"
